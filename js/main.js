bab4ca0330929a0bcd2634845a63deef9042c4a8
Nguyen Gia Linh <nguyengialinh@Nguyens-MacBook-Pro.local>
2022-01-30 06:13:50 +0700

    fix function

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 (()=>{
     let theHeader =  document.querySelector("#mainNav");
+     theSVG =  document.querySelector("#icon-holder");
+
+
     function logElement(){
-        console.log('clicked on header navigation');
+        console.log('clicked on button');
     }
     theHeader.addEventListener("click", logElement );
-
-    let theSVG =  document.querySelector(".wrapper img");
-    function logElement(){
-        console.log('clicked on icons');
-    }
     theSVG.addEventListener("click", logElement );
 
+    
 
 })();
+// let theSVG =  document.querySelector(".wrapper img");
+//     function logElement(){
+//         console.log('clicked on icons');
+//     }
+//     theSVG.addEventListener("click", logElement );
